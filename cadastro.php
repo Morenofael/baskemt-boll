5317c100d0205559d6c6d4935a32359b300dcca7
Y0r10 <101215431+Y0r10@users.noreply.github.com>
2023-06-26 14:59:25 -0300

    Colocando Form e Estilizando
    Form passado para essá página apenas,e colocado link do css.

--- cadastro.php
+++ cadastro.php
@@ -32,96 +32,143 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="index.css">
 </head>
 <body>
+    <nav id="navbar" class="navbar navbar-expand-lg bg-body-tertiary mb-1" data-bs-theme="dark">
+      <div class="container-fluid">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <img src="./imagens/logo-nba.png" alt="Logo da NBA">
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <a class="navbar-brand" href="#">NBA-Trade</a>
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link" href="https://github.com/Morenofael/baskemt-boll">GitHub</a>
+            </li>
+          </ul>
+          <form class="d-flex" role="search">
+            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+            <button class="btn btn-outline-success" type="submit">Search</button>
+          </form>
+        </div>
+      </div>
+  </nav>
+
+<div class="container">
  
     <form action="" method="POST">
-      
-      <input type="text" name="nome" 
-          placeholder="Informe o Nome do Jogador" />
-          
-      <br>
+        <div class="mb-3">
+            <label for="inputNome" class="form-label">Nome do Jogador</label>
+                <input id="inputNome" class="form-control" type="text" name="nome">
+        </div> 
 
-      <input type="number" name="altura"
-          placeholder="Informe a altura do Jogador" />
+            <br>
 
-      <br>
+        <div class="mb-3">
+            <label for="inputAlt" class="form-label">Altura do Jogador</label>
+            <input id="inputAlt" class="form-control" type="number" name="altura">
+        </div>
 
-      <input type="text" name="peso"
-          placeholder="Informe o peso do Jogador" />
+        <br>
 
-      <br>
+        <div class="mb-3">
+            <label for="inputPeso" class="form-label">Peso do Jogador</label>
+            <input id="inputPeso" class="form-control" type="text" name="peso">
+        </div>
 
-      <input type="date" name="nascimento"
-          placeholder="Informe a data de nascimento do Jogador" />
+            <br>
 
-      <br>
+        <div class="mb-3">
+            <label for="inputNas" class="form-label">Data de Nascimento do Jogador</label>
+            <input id="inputNas" class="form-control" type="date" name="nascimento">
+        </div>
 
-      <input type="text" name="universidade"
-          placeholder="Informe a Universidade do Jogador" />
+            <br>
 
-      <br>
+        <div class="mb-3">
+            <label for="inputUni" class="form-label">universidade que o Jogador atuou</label>
+            <input id="inputUni" class="form-control" type="text" name="universidade">
+        </div>
 
-      <input type="number" name="pickYear"
-          placeholder="Informe o ano de Draft do Jogador" />
+            <br>
 
-      <br>
+            <input type="number" name="pickYear"
+                placeholder="Informe o ano de Draft do Jogador" />
 
-      <input type="number" name="pickRd"
-          placeholder="Informe o round de Draft do Jogador" />
+            <br>
 
-      <br>
+            <input type="number" name="pickRd"
+                placeholder="Informe o round de Draft do Jogador" />
 
-      <input type="number" name="pickNum"
-          placeholder="Informe o número da pick de Draft do Jogador" />
+            <br>
 
-      <br>
+            <input type="number" name="pickNum"
+                placeholder="Informe o número da pick de Draft do Jogador" />
 
-      <select name="pickTeam">
-          <option value="">---TIME DE DRAFT---</option>
-          <option value="GSW">Golden State Warriors</option>
-          <option value="LAL">Los Angeles Lakes</option>
-          <option value="DEN">Denver Nuggets</option>
-          <option value="LAC">LA Clippers</option>
-          <option value="PHX">Phoenix Suns</option>
-          <option value="SAC">Sacramento Kings</option>
-          <option value="BOS">Boston Celtics</option>
-          <option value="BKN">Brooklyn Nets</option>
-          <option value="NYK">New York Knicks</option>
-          <option value="P76">Philadelphia 76ers</option>
-          <option value="TOR">Toronto Raptors</option>
-          <option value="CHI">Chicago Bulls</option>
-          <option value="CLE">Cleveland Cavaliers</option>
-          <option value="DET">Detroit Pistons</option>
-          <option value="IND">Indiana Pacers</option>
-          <option value="MIL">Milwaukee Bucks</option>
-          <option value="ATL">Atlanta Hawks</option>
-          <option value="CHA">Charlotte Hornets</option>
-          <option value="MIA">Miami Heat</option>
-          <option value="ORL">Orlando Magic</option>
-          <option value="WAS">Washington Wizards</option>
-          <option value="MIN">Minnesota Timberwolves</option>
-          <option value="OKC">Oklahoma City Thunder</option>
-          <option value="POR">Portland Trail Blazers</option>
-          <option value="UTA">Utah Jazz</option>
-          <option value="DAL">Dallas Mavericks</option>
-          <option value="HOU">Houston Rockets</option>
-          <option value="MEM">Memphis Grizzlies</option>
-          <option value="NOP">New Orleans Pelicans</option>
-          <option value="SAS">San Antonio Spurs</option>
-      </select>
+            <br>
 
-      <br>
+            <select name="pickTeam">
+                <option value="">---TIME DE DRAFT---</option>
+                <option value="GSW">Golden State Warriors</option>
+                <option value="LAL">Los Angeles Lakes</option>
+                <option value="DEN">Denver Nuggets</option>
+                <option value="LAC">LA Clippers</option>
+                <option value="PHX">Phoenix Suns</option>
+                <option value="SAC">Sacramento Kings</option>
+                <option value="BOS">Boston Celtics</option>
+                <option value="BKN">Brooklyn Nets</option>
+                <option value="NYK">New York Knicks</option>
+                <option value="P76">Philadelphia 76ers</option>
+                <option value="TOR">Toronto Raptors</option>
+                <option value="CHI">Chicago Bulls</option>
+                <option value="CLE">Cleveland Cavaliers</option>
+                <option value="DET">Detroit Pistons</option>
+                <option value="IND">Indiana Pacers</option>
+                <option value="MIL">Milwaukee Bucks</option>
+                <option value="ATL">Atlanta Hawks</option>
+                <option value="CHA">Charlotte Hornets</option>
+                <option value="MIA">Miami Heat</option>
+                <option value="ORL">Orlando Magic</option>
+                <option value="WAS">Washington Wizards</option>
+                <option value="MIN">Minnesota Timberwolves</option>
+                <option value="OKC">Oklahoma City Thunder</option>
+                <option value="POR">Portland Trail Blazers</option>
+                <option value="UTA">Utah Jazz</option>
+                <option value="DAL">Dallas Mavericks</option>
+                <option value="HOU">Houston Rockets</option>
+                <option value="MEM">Memphis Grizzlies</option>
+                <option value="NOP">New Orleans Pelicans</option>
+                <option value="SAS">San Antonio Spurs</option>
+            </select>
 
-      <input type="text" name="fotoLink"
-          placeholder="Adicione um link de foto do Jogador" />
+            <br>
 
-      <br>
+            <input type="text" name="fotoLink"
+                placeholder="Adicione um link de foto do Jogador" />
 
-      <button type="submit">Cadastrar</button>
+            <br>
 
-      <input type="hidden" name="submetido" value="1" />
-  </form>
-     
+            <button type="submit">Cadastrar</button>
+
+            <input type="hidden" name="submetido" value="1" />
+    </form>
+    </div>
+    <footer class="bg-dark">
+      <div class="container-fluid d-flex text-light mt-4 pt-4">
+        <img src="imagens/logo-nba.png" alt="Logo da NBA" class="img-fluid">
+        <div>
+            <p>Sobre nós</p>
+            <p>FAQ</p>
+            <p>Jogadores</p>
+            <p>Jurídico</p>
+            <button type="button" class="btn btn-outline-success"><a href="index.php">Voltar para a página anterior</a></button>
+        </div>
+      </div>
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"></script>
+
 </body>
 </html>   
